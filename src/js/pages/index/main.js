761de3ce18d0ac90e25ca6cd0d7beb230c4fa380
kylehovey <khoveyc@gmail.com>
2017-10-25 14:17:11 -0600

    Added file upload.

--- src/js/pages/index/main.js
+++ src/js/pages/index/main.js
@@ -12,5 +12,39 @@ $(() => {
   $.fn.fullpage.setAllowScrolling(false, "down, up");
 
   // ===== Listeners =====
-  $("#upload-json").on("click", (e) => { });
+  $("#upload-json").on("click", (e) => {
+    $("#upload-json-input").trigger("click");
+  });
+
+  $("#upload-json-input").on("change", (e) => {
+    // Start loading animation
+    const upload = $("#upload-json");
+    upload.button("loading");
+
+    // Get the file
+    const [ fileInput ] = $(e.target);
+    const [ file ] = fileInput.files;
+
+    // Create a file reader
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      // Get raw data
+      const data = JSON.parse(e.target.result);
+
+      // Set data of trip analyser
+      app.analyser.setData(data);
+
+      // Reset loader view
+      await upload.fadeOut().promise();
+
+      // Done!
+      $("#upload-done")
+        .hide()
+        .removeClass("hidden")
+        .fadeIn();
+    }
+
+    // Read the file
+    reader.readAsText(file);
+  });
 });
