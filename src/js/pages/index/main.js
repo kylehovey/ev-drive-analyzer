43717265fdb6c5f7cd9a6dd2d2bafd49541f4796
kylehovey <khoveyc@gmail.com>
2017-10-25 13:03:40 -0600

    Added full page scroll library.

--- src/js/pages/index/main.js
+++ src/js/pages/index/main.js
@@ -4,4 +4,13 @@ $(() => {
 
   // Initialize data
   window.app.init();
+
+  // Initialize fullpage
+  $("#fullpage").fullpage();
+
+  // Disable scrolling
+  $.fn.fullpage.setAllowScrolling(false, "down, up");
+
+  // ===== Listeners =====
+  $("#upload-json").on("click", (e) => { });
 });
