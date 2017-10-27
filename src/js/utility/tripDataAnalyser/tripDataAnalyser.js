d098afb6f1d0148fe65912914c0e5d4114e87dee
kylehovey <khoveyc@gmail.com>
2017-10-27 17:34:37 -0600

    Updated trip calculation transform.

--- src/js/utility/tripDataAnalyser/tripDataAnalyser.js
+++ src/js/utility/tripDataAnalyser/tripDataAnalyser.js
@@ -49,23 +49,40 @@ class TripDataAnalyser {
   /**
    * Compute all trips in a recent time period from the data
    *  - A trip is defined as an ordered set of {lat,lon,elev,date} locations
-   *    such that the adjacent pairwise geographic distance between the former
+   *    such that the adjacent pairwise time difference between the former
    *    and latter location does not exceed some epsilon deemed unreasonable
-   *    for a regular sampling of vehicular travel data. An epsilon for time
-   *    is also considered.
+   *    for a regular sampling of vehicular travel data.
    *  @param {Number} days Number of days to go back from now in analysis
-   *  @param {Number} maxDistDelta Location epsilon mentioned in definition
-   *    of trip (mph)
    *  @param {Number} maxTDelta Temporal epsilon mentioned in definition (hours)
    *  @param {Number} minTripDist Minimum distance in miles for trip results
+   *  @param {Number} minDistDelta Minimum decimal degree difference to warrant
+   *    considering a pair of points as being different locations geographically
    */
-  _computeTrips(days = 90, maxDistDelta = 4, maxTDelta = 1, minTripDist = 0.5) {
+  _computeTrips(days = 90, maxTDelta = 0.1, minTripDist = 0.5, minDistDelta = 0.00001) {
     // Get data from the specified time period
     const dateCutoff = +new Date - days * 86400000;
 
-    const rawData = this._opts.takeoutData
-      .filter(loc => +loc.timestampMs > dateCutoff)
-      .map(loc => new Location(loc));
+    // Get recent data and transform it into Array of Locations
+    let rawData = _(this._opts.takeoutData)
+      .filter(geoJSON => +geoJSON.timestampMs > dateCutoff)
+      .map(geoJSON => new Location(geoJSON))
+      .value();
+
+    // Exclude adjacent points that are very close to each other
+    rawData = _(rawData)
+      .filter((loc, i) => {
+        if (rawData[i + 1] !== undefined) {
+          const [ [lon1, lat1], [lon2, lat2] ] = [ loc, rawData[i + 1] ]
+            .map(l => l.toArray());
+
+          return [ [lon1, lon2], [lat1, lat2] ]
+            .filter(([a, b]) => Math.abs(a - b) < minDistDelta)
+            .length === 0;
+        } else {
+          return true;
+        }
+      })
+      .value()
 
     // Organize data into trips
     this._trips = [ new Trip ];
@@ -74,10 +91,7 @@ class TripDataAnalyser {
 
     const maxTDeltaMs = maxTDelta * 3600000;
     rawData.forEach(curLoc => {
-      if (
-        turf.distance(lastLoc.coords, curLoc.coords, 'miles') > maxDistDelta
-        || (curLoc.getTime() - lastLoc.getTime() > maxTDeltaMs)
-      ) {
+      if (curLoc.getTime() - lastLoc.getTime() > maxTDeltaMs) {
         // New trip
         curTrip = new Trip;
         this._trips.push(curTrip);
