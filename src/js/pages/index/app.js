ad6a78a3b0652fe0e3cacc8ca21e7355ec908ae3
kylehovey <khoveyc@gmail.com>
2017-11-02 18:04:36 -0600

    Added Google Charts.

--- src/js/pages/index/app.js
+++ src/js/pages/index/app.js
@@ -25,19 +25,47 @@ class App {
   
     // Mapbox Config
     mapboxgl.accessToken =
-      'pk.eyJ1IjoidXBlbCIsImEiOiJjajllZ29reTUyYTJoMndsc3ZtdGg2NXpsIn0.Y6sKlsUA9ZIm8rHfklQPaQ';
+      "pk.eyJ1IjoidXBlbCIsImEiOiJjajllZ29reTUyYTJoMndsc3ZtdGg2NXpsIn0.Y6sKlsUA9ZIm8rHfklQPaQ";
 
     /**
      * @type {mapboxgl.Map}
      */
     this.map = new mapboxgl.Map({
-      container : 'map',
-      style : 'mapbox://styles/mapbox/light-v9',
+      container : "map",
+      style : "mapbox://styles/mapbox/light-v9",
       center: [-111.801, 41.746],
       zoom: 9
     });
 
     // Config
     this.map.addControl(new mapboxgl.NavigationControl());
+
+    // Google chart loading
+    google.charts.load("current", { "packages" : [ "corechart" ] }).then(() => {
+      const data = google.visualization.arrayToDataTable([
+        ["", "", { role: "style" }, { role: 'annotation' }],
+        ["Trips Possible With EV", 600, "#0091ba", 600],
+        ["Trips That Require Fill-Up", 100, "salmon", 100]
+      ]);
+
+      const options = {
+        title: "",
+        hAxis: {
+          minValue: 0,
+          textPosition : "none",
+          gridlines : { color : "transparent" }
+        },
+        vAxis : {
+          textPosition : "none",
+          gridlines : { color : "transparent" }
+        },
+        legend: { position: "none" },
+        backgroundColor : { fill:"transparent" }
+      };
+
+      const chart = new google.visualization.BarChart($("#chart")[0]);
+
+      chart.draw(data, options);
+    });
   }
 };
